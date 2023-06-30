e7d4c0d0ad154afeb2d95eefda1783091364a329
luu0124 <138016597+luu0124@users.noreply.github.com>
2023-06-30 18:26:05 +0800

    06300621

--- app_v1/Sql_FAILURE_v7/Sql_FAILURE/app/src/main/java/com/example/sql_failure/check_fragment/BookFragment.java
+++ app_v1/Sql_FAILURE_v7/Sql_FAILURE/app/src/main/java/com/example/sql_failure/check_fragment/BookFragment.java
@@ -47,6 +47,7 @@ public class BookFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+//test
 //pp
     @Override
     public void onCreate(Bundle savedInstanceState) {
